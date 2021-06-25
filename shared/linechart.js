a173b6ca26e66404d16ab9b2c1485aab009ea5e7
Roxane <roxelo@users.noreply.github.com>
2021-06-25 14:43:41 -0400

    Update shared/linechart.js
    Co-authored-by: jaxonL <jaxon.lin@uwaterloo.ca>

--- shared/linechart.js
+++ shared/linechart.js
@@ -98,7 +98,8 @@ function renderLineChart(elem, data, width, height, options) {
         })
         .attr("x", 40)
         .attr("dy", "0.35em")
-        .attr("fill", function (d) {
+        .attr("class", "line-chart-label")
+        .style("fill", function (d) {
           return colorScale(i);
         })
         .text(function(d) { return d.location; });
