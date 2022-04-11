59775f7fc0637c39dbe4e70c53527c936e2c3fd5
wesley-a-leung <wesley.a.leung@gmail.com>
2022-04-11 19:32:43 -0400

    Added covid data

---
+++ data/covid.ts
@@ -0,0 +1,91 @@
+// Updated for 2022
+let CONTRACTED_COVID = [
+  {
+    "name": "No",
+    "value": 47
+  },
+  {
+    "name": "Once",
+    "value": 11
+  },
+  {
+    "name": "More than once",
+    "value": 1
+  }
+];
+
+let KNOW_SOMEONE_CONTRACTED_COVID = [
+  {
+    "name": "Yes",
+    "value": 56
+  },
+  {
+    "name": "No",
+    "value": 3
+  }
+];
+
+let COVID_DOSES = [
+  {
+    "name": "0",
+    "value": 0
+  },
+  {
+    "name": "1",
+    "value": 0
+  },
+  {
+    "name": "2",
+    "value": 16
+  },
+  {
+    "name": "3",
+    "value": 42
+  },
+  {
+    "name": "4 or more",
+    "value": 1
+  }
+];
+
+let COVID_TESTS = [
+  {
+    "name": "tests",
+    "value": [1, 3, 1, 0, 6, 18, 3, 4, 12, 5, 0, 15, 1, 3, 50, 0, 4, 0, 5, 10, 15, 1, 5, 2, 20, 40, 1, 3, 18, 4, 0, 5, 10, 3, 50, 2, 4, 2, 9, 4, 2, 2, 1, 4, 10, 2, 20, 0, 0, 5, 5, 12, 4, 0, 58, 0, 6]
+  }
+];
+
+let FOLLOW_PUBLIC_GUIDANCE = [
+  {
+    "name": "Never",
+    "value": 1
+  },
+  {
+    "name": "Sometimes",
+    "value": 6
+  },
+  {
+    "name": "Most of the time",
+    "value": 42
+  },
+  {
+    "name": "Always",
+    "value": 10
+  }
+];
+
+let LARGEST_GATHERING = [
+  {
+    "name": "largest gathering",
+    "value": [4, 50000, 10, 10, 80, 70, 20, 50, 10, 400000, 10, 10, 30, 200, 50, 400000, 30, 100, 5, 5, 100, 15, 60, 10, 15, 120, 80, 75, 200, 5, 25, 75, 30, 80, 10, 3800, 100, 15, 80, 100, 20, 10, 50, 1, 8, 10, 8, 15, 6, 200, 60, 60, 10, 100, 25]
+  }
+];
+
+export {
+  CONTRACTED_COVID,
+  KNOW_SOMEONE_CONTRACTED_COVID,
+  COVID_DOSES,
+  COVID_TESTS,
+  FOLLOW_PUBLIC_GUIDANCE,
+  LARGEST_GATHERING
+}
