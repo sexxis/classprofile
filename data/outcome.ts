ca70488c11a11953712fdc3704a71fb1c9a79120
wesley-a-leung <wesley.a.leung@gmail.com>
2022-04-14 17:59:44 -0400

    Updated outcomes for 2022

--- data/outcome.ts
+++ data/outcome.ts
@@ -1,48 +1,167 @@
-let ORIGINAL = [{
-  "name": "Yes",
-  "value": 95
-}, {
-  "name": "No",
-  "value": 18
-}]
-
-
-let CHOOSE_PROGRAM = [{
-  "name": "Architecture",
-  "value": 1
-  }, {
-  "name": "English",
-  "value": 1
-  }, {
-  "name": "Fine Arts",
-  "value": 1
-  }, {
-  "name": "Mechatronics Engineering",
-  "value": 1
-  }, {
-  "name": "Systems Design Engineering",
-  "value": 1
-  }, {
-  "name": "Computer Science",
-  "value": 29
-  }, {
-  "name": "Software Engineering",
-  "value": 79
-}]
-
-let GENDER_RATING = [
+// updated for 2022
+let COURSE_WITH_LARGEST_WORKLOAD = [
   {
-    "name": "Men",
-    "value": [2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    "name": "course with largest workload",
+    "value": ['SE 490', 'CS 343', 'CS 343', 'CS 343', 'SE 350', 'CS 343', 'SE 490', 'CS 488', 'CS 343', 'SE 350', 'SE 380', 'MATH 239', 'SE 350', 'SE 350', 'SE 380', 'SE 464', 'CS 343', 'CS 452', 'SE 350', 'MATH 213', 'SE 350', 'SE 350', 'SE 350', 'SE 350', 'SE 350', 'CS 343', 'CS 343', 'CS 343', 'CS 452', 'CS 343', 'SE 350', 'SE 350', 'CS 343', 'CS 343', 'CS 444', 'CS 343', 'CS 343', 'SE 350', 'CS 343', 'CS 241', 'SE 350', 'CS 488', 'CS 343', 'MATH 239', 'GEOG 207']
+  }
+];
+
+let BURNT_OUT = [
+  {
+    "name": "Yes (SE)",
+    "value": 20
   },
   {
-    "name": "Women",
-    "value": [3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    "name": "No (SE)",
+    "value": 33
+  },
+  {
+    "name": "Unsure (SE)",
+    "value": 5
+  },
+  {
+    "name": "Yes (Ex-SE)",
+    "value": 0
+  },
+  {
+    "name": "No (Ex-SE)",
+    "value": 3
+  },
+  {
+    "name": "Unsure (Ex-SE)",
+    "value": 0
+  }
+];
+
+let SENIORITIS = [
+  {
+    "name": "Yes (SE)",
+    "value": 33
+  },
+  {
+    "name": "No (SE)",
+    "value": 12
+  },
+  {
+    "name": "Unsure (SE)",
+    "value": 4
+  },
+  {
+    "name": "Yes (Ex-SE)",
+    "value": 2
+  },
+  {
+    "name": "No (Ex-SE)",
+    "value": 1
+  },
+  {
+    "name": "Unsure (Ex-SE)",
+    "value": 0
+  }
+];
+
+let P_ENG = [
+  {
+    "name": "Yes (SE)",
+    "value": 5
+  },
+  {
+    "name": "No (SE)",
+    "value": 40
+  },
+  {
+    "name": "Unsure (SE)",
+    "value": 13
+  },
+  {
+    "name": "Yes (Ex-SE)",
+    "value": 0
+  },
+  {
+    "name": "No (Ex-SE)",
+    "value": 2
+  },
+  {
+    "name": "Unsure (Ex-SE)",
+    "value": 1
+  }
+];
+
+let CONSIDERED_DROPPING_OUT_OF_SE = [
+  {
+    "name": "Yes",
+    "value": 17
+  },
+  {
+    "name": "No",
+    "value": 40
+  },
+  {
+    "name": "Unsure",
+    "value": 1
+  }
+];
+
+let PROGRAM_TO_DROP_TO = [
+  {
+    "name": "Computer Science",
+    "value": 17
+  },
+  {
+    "name": "Political Science",
+    "value": 1
+  },
+  {
+    "name": "Chemistry/Physics",
+    "value": 1
+  }
+];
+
+let CONSIDERED_DROPPING_OUT_OF_UNI = [
+  {
+    "name": "Yes",
+    "value": 1
+  },
+  {
+    "name": "No",
+    "value": 56
+  },
+  {
+    "name": "Unsure",
+    "value": 0
+  }
+];
+
+let START_AGAIN_PROGRAM = [
+  {
+    "name": "SE",
+    "value": ['Waterloo SE', 'Waterloo SE', 'CS+English', 'MIT', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo CS', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo CS', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo CS', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE', 'Waterloo SE']
+  },
+  {
+    "name": "Ex-SE",
+    "value": ['Waterloo SE', 'Waterloo SE', 'Waterloo SE']
+  }
+];
+
+let HAPPY_WITH_CHOICE = [
+  {
+    "name": "SE",
+    "value": [4, 5, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 5, 5, 3, 5, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, 4, 4, 5, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    "name": "Ex-SE",
+    "value": [4, 5, 5]
   }
 ];
 
 export {
-  ORIGINAL,
-  CHOOSE_PROGRAM,
-  GENDER_RATING
+  COURSE_WITH_LARGEST_WORKLOAD,
+  BURNT_OUT,
+  SENIORITIS,
+  P_ENG,
+  CONSIDERED_DROPPING_OUT_OF_SE,
+  PROGRAM_TO_DROP_TO,
+  CONSIDERED_DROPPING_OUT_OF_UNI,
+  START_AGAIN_PROGRAM,
+  HAPPY_WITH_CHOICE
 }
